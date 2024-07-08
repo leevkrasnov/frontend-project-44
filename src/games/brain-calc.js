dbf3a3ef2008480d6a6480f2d21c24e25123fb37
lev krasnov <leevkrasnov@gmail.com>
2024-07-08 13:56:35 +0500

    fix brain-calc 2.0

--- src/games/brain-calc.js
+++ src/games/brain-calc.js
@@ -1,19 +1,31 @@
 import { getRandomNumber } from "../engine.js";
 import { startBrainGames } from "../index.js";
-import { create, all } from 'mathjs';
 
-const beginsGreet = 'What is the result of the expression?'
+const beginsGreet = "What is the result of the expression?";
+
+const doArithmetic = (num1, num2, operator = "+") => {
+  switch (operator) {
+    case "+":
+      return num1 + num2;
+    case "-":
+      return num1 - num2;
+    case "*":
+      return num1 * num2;
+    default:
+      throw new Error(`Unexpected operator: ${operator}`);
+  }
+};
 
 const main = () => {
-  const math = create(all)
-  const symbols = ['*', '-', '+']
+  const symbols = ["*", "-", "+"];
   const numFirst = getRandomNumber(1, 100);
   const numSecond = getRandomNumber(1, 100);
-  const symbol = symbols[getRandomNumber(0, 2)]
-  const question = `${numFirst} ${symbol} ${numSecond}`
-  const correctAnswer = math.evaluate(`${numFirst} ${symbol} ${numSecond}`);
+  const symbol = symbols[getRandomNumber(0, 2)];
+
+  const question = `${numFirst} ${symbol} ${numSecond}`;
+  const correctAnswer = doArithmetic(numFirst, numSecond, symbol);
   return [question, String(correctAnswer)];
-}
+};
 
 const runGameCalc = () => startBrainGames(beginsGreet, main);
 export default runGameCalc;
